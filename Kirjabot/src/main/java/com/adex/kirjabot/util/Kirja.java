876969179ea59147b85ce651c261b7bcf534757b
adex720 <amauno@live.com>
2021-01-07 16:02:54 +0200

    Update Kirja.java

--- Kirjabot/src/main/java/com/adex/kirjabot/util/Kirja.java
+++ Kirjabot/src/main/java/com/adex/kirjabot/util/Kirja.java
@@ -3,17 +3,23 @@ package com.adex.kirjabot.util;
 public class Kirja {
 
     private final String nimi;
+    private final String lukija;
     private final String kirjailija;
     private final int sivut;
 
-    public Kirja(String nimi, String kirjailija, int sivut) {
+    public Kirja(String nimi, String lukija, String kirjailija, int sivut) {
         this.nimi = nimi;
+        this.lukija = lukija;
         this.kirjailija = kirjailija;
         this.sivut = sivut;
     }
 
     public String getnimi() {
         return nimi;
+    }
+
+    public String getLukija() {
+        return lukija;
     }
 
     public int getSivut() {
@@ -26,6 +32,6 @@ public class Kirja {
 
     @Override
     public String toString() {
-        return nimi + ", " + kirjailija + "; " + sivut + " sivua";
+        return lukija + ": " + nimi + ", " + kirjailija + "; " + sivut + " sivua";
     }
 }
