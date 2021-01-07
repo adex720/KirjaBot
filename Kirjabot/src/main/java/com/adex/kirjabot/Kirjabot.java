5e48cec5ee4b03b5b6a2157944cda7a84f229d5f
adex720 <amauno@live.com>
2021-01-07 16:44:43 +0200

    Update Kirjabot.java

--- Kirjabot/src/main/java/com/adex/kirjabot/Kirjabot.java
+++ Kirjabot/src/main/java/com/adex/kirjabot/Kirjabot.java
@@ -2,11 +2,11 @@ package com.adex.kirjabot;
 
 import com.adex.kirjabot.commands.Command;
 import com.adex.kirjabot.commands.CommandExecutor;
+import com.adex.kirjabot.commands.ListaaKirjat;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 import java.util.ArrayList;
@@ -14,24 +14,23 @@ import java.util.ArrayList;
 public class Kirjabot {
 
     public static final String PREFIX = "!";
-
     public static final ArrayList<Command> COMMANDS = new ArrayList<>();
 
+    
     private static final Dotenv dotenv = Dotenv.load();
-
     public static JDA builder;
 
     public static void main(String[] args) throws LoginException {
 
-
         builder = JDABuilder.createDefault(dotenv.get("TOKEN"))
-                .addEventListeners(new CommandExecutor())
+                .addEventListeners(new CommandExecutor()/*, new JoinLeaveListener(), new ReactionListener()*/)
                 .setActivity(Activity.playing("Reads books"))
                 .build();
 
+        Loader.onInitialize();
+
+
+        new ListaaKirjat();
     }
 
-    public static void addListener(ListenerAdapter listener){
-        builder.addEventListener(listener);
-    }
 }
