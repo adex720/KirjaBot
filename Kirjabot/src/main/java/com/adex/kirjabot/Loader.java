33deaf2edbc623dc26a39c99338b789186f96b5d
adex720 <amauno@live.com>
2021-01-07 16:02:38 +0200

    Update Loader.java

--- Kirjabot/src/main/java/com/adex/kirjabot/Loader.java
+++ Kirjabot/src/main/java/com/adex/kirjabot/Loader.java
@@ -8,20 +8,27 @@ import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Loader {
 
     public final static ArrayList<Kirja> KIRJAT = new ArrayList<>();
 
-    public static void onInitialize(){
+    public static final HashMap<Integer, String> NIMET = new HashMap<>();
+
+    public static void onInitialize() {
+        NIMET.put(1, "Aapo");
+        NIMET.put(2, "Juuso");
+        NIMET.put(3, "Jan");
+        NIMET.put(4, "Viljami");
         lataaKirjat();
     }
 
-    public static void lataaKirjat(){
+    public static void lataaKirjat() {
         ArrayList<JSONObject> kirjaLista = new ArrayList<>();
-        
 
-        try{
+
+        try {
             ClassLoader cl = Thread.currentThread().getContextClassLoader();
             URL url = cl.getResource("kirjat.json");
             assert url != null;
@@ -29,26 +36,26 @@ public class Loader {
 
             Object obj = new JSONParser().parse(new FileReader(wordList));
             JSONObject kirjat = (JSONObject) obj;
-            
-            kirjaLista.add((JSONObject) kirjat.get("aapo"));
-            kirjaLista.add((JSONObject) kirjat.get("juuso"));
-            kirjaLista.add((JSONObject) kirjat.get("jan"));
-            kirjaLista.add((JSONObject) kirjat.get("viljami"));
-        } catch (Exception e){
+
+            kirjaLista.add((JSONObject) kirjat.get(NIMET.get(1)));
+            kirjaLista.add((JSONObject) kirjat.get(NIMET.get(2)));
+            kirjaLista.add((JSONObject) kirjat.get(NIMET.get(3)));
+            kirjaLista.add((JSONObject) kirjat.get(NIMET.get(4)));
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        for (JSONObject jsonObject : kirjaLista){
+        int i = 1;
+        for (JSONObject jsonObject : kirjaLista) {
             try {
                 String nimi = jsonObject.get("nimi").toString();
                 String kirjailija = jsonObject.get("kirjailija").toString();
                 int sivut = Integer.parseInt(jsonObject.get("sivut").toString());
 
-                KIRJAT.add(new Kirja(nimi, kirjailija, sivut));
-            } catch (Exception e){
+                KIRJAT.add(new Kirja(nimi, NIMET.get(i), kirjailija, sivut));
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
-
+            i++;
         }
     }
 }
