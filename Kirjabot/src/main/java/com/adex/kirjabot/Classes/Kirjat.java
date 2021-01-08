24668d54667c224e8029b98faf977ef74cf3ae58
adex720 <amauno@live.com>
2021-01-08 19:47:42 +0200

    Update Kirjat.java

--- Kirjabot/src/main/java/com/adex/kirjabot/Classes/Kirjat.java
+++ Kirjabot/src/main/java/com/adex/kirjabot/Classes/Kirjat.java
@@ -1,11 +1,13 @@
-Package com.adex.kirjabot.classes;
+package com.adex.kirjabot.classes;
 
-import java.util.Arraylist;
+import com.adex.kirjabot.util.Kirja;
+
+import java.util.ArrayList;
 
 public class Kirjat {
 
     public static final ArrayList<Kirja> KIRJAT = new ArrayList<>();
-        
+
     public static Kirja get(String lukija){
         for(Kirja kirja : KIRJAT){
             if(kirja.getLukija().equalsIgnoreCase(lukija)) return kirja;
